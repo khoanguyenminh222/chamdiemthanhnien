f211c1a39f70aa8a7c8c665f54008a7d6c747418
Khoa Minh <khoanguyenminh222@gmail.com>
2024-03-19 11:26:35 +0700

    up 2

--- WebApplication1/Controllers/loaiTieuChiController.cs
+++ WebApplication1/Controllers/loaiTieuChiController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Ajax.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
@@ -17,6 +18,41 @@ namespace WebApplication1.Controllers
         // GET: loaiTieuChi
         public ActionResult Index()
         {
+            var tieuChi_loaiTieuChi = (from loaiTieuChi in db.loaiTieuChis
+                                       join nhomChiTieu in db.nhomChiTieux
+                                        on loaiTieuChi.iD equals nhomChiTieu.fk_loaiTieuChi
+                                       join chiTieu in db.chiTieux
+                                        on nhomChiTieu.iD equals chiTieu.fk_loaiChiTieu
+                                      select new dataBangDiem
+                                      {
+                                          chiTieu = chiTieu,
+                                          loaiTieuChi = loaiTieuChi,
+                                      }).OrderBy(l=>l.loaiTieuChi.iD).DistinctBy(l=>l.loaiTieuChi.iD);
+            var tieuChi_giaoChiTieu = (from giaoChiTieuchoDV in db.giaoChiTieuchoDVs
+                                       join chiTieu in db.chiTieux
+                                        on giaoChiTieuchoDV.fk_chiTieu equals chiTieu.iD
+                                       join nhomChiTieu in db.nhomChiTieux
+                                        on chiTieu.fk_loaiChiTieu equals nhomChiTieu.iD
+                                       join loaiTieuChi in db.loaiTieuChis
+                                        on nhomChiTieu.fk_loaiTieuChi equals loaiTieuChi.iD
+                                       join dm_donVi in db.dm_donVi
+                                        on giaoChiTieuchoDV.fk_dmDonVi equals dm_donVi.iD
+                                       join nguoiDung in db.nguoiDungs
+                                        on dm_donVi.fk_nguoiQuanLy equals nguoiDung.iD
+                                       join donVi in db.donVis
+                                        on nguoiDung.fk_donVi equals donVi.iD
+                                       select new dataBangDiem
+                                       {
+                                           giaoChiTieuchoDV = giaoChiTieuchoDV,
+                                           loaiTieuChi = loaiTieuChi,
+                                           chiTieu = chiTieu,
+                                           dm_DonVi = dm_donVi,
+                                           nguoiDung = nguoiDung,
+                                           donVi = donVi,
+                                       }).DistinctBy(c=>c.giaoChiTieuchoDV.fk_chiTieu);
+  
+            ViewBag.tieuChi_loaiTieuChi = tieuChi_loaiTieuChi.ToList();
+            ViewBag.tieuChi_giaoChiTieu = tieuChi_giaoChiTieu.ToList();
             return View(db.loaiTieuChis.ToList());
         }
 
@@ -70,6 +106,17 @@ namespace WebApplication1.Controllers
             {
                 return HttpNotFound();
             }
+            var nguoiDungList = (from dm_donVi in db.dm_donVi
+                                 join nguoiDung in db.nguoiDungs
+                                    on dm_donVi.fk_nguoiQuanLy equals nguoiDung.iD
+                                 join donVi in db.donVis
+                                    on nguoiDung.fk_donVi equals donVi.iD
+                                 select new
+                                 {
+                                     iD = dm_donVi.iD,
+                                     tenNguoiDung = nguoiDung.ten + "/ "+ donVi.ten,
+                                 }).ToList();
+            ViewBag.nguoiDungList = new SelectList(nguoiDungList, "iD", "tenNguoiDung");
             return View(loaiTieuChi);
         }
 
@@ -78,15 +125,49 @@ namespace WebApplication1.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "iD,ten,tongDiem")] loaiTieuChi loaiTieuChi)
+        public ActionResult Edit([Bind(Include = "iD,ten,tongDiem")] loaiTieuChi loaiTieuChi,
+                                  int dm_DonVi)
         {
-            //if (ModelState.IsValid)
-            //{
-            //    db.Entry(loaiTieuChi).State = EntityState.Modified;
-            //    db.SaveChanges();
-            //    return RedirectToAction("Index");
-            //}
-            return View(loaiTieuChi);
+            //loaiTieuChi loaiTieuChi = db.loaiTieuChis.Find(loaiTieuChi.iD);
+            var nhomChiTieu = db.nhomChiTieux.Where(n => n.fk_loaiTieuChi == loaiTieuChi.iD).ToList();
+            var giaoChiTieuchoDV = db.giaoChiTieuchoDVs.ToList();
+            foreach (var nhom in nhomChiTieu)
+            {
+                var chiTieu = db.chiTieux.Where(c => c.fk_loaiChiTieu == nhom.iD).ToList();
+                //kiểm tra chỉ tiêu có nằm trong bảng giaoChiTieuchoDV, nếu có thì không tạo mới
+                foreach (var chi in chiTieu)
+                {
+                    foreach (var giaoChiTieu in giaoChiTieuchoDV)
+                    {
+                        if (chi.iD == giaoChiTieu.fk_chiTieu)
+                        {
+                            //id bằng nghĩa là có trong bảng giaoChiTieu rồi
+                            //update fk_dm đơn vị thôi
+                            giaoChiTieu.fk_dmDonVi = dm_DonVi;
+                            db.SaveChanges();
+                            break;
+                        }
+                        else
+                        {
+                            // tạo nhiều giaoChiTieuchoDV với nhiều chỉ tiêu
+                            
+                            giaoChiTieuchoDV newGiao = new giaoChiTieuchoDV();
+                            newGiao.fk_chiTieu = chi.iD;
+                            newGiao.fk_dmDonVi = dm_DonVi;
+                            db.giaoChiTieuchoDVs.Add(newGiao);
+                            db.SaveChanges();
+
+                            //tạo bảng điểm
+                            bangDiem newBang = new bangDiem();
+                            newBang.fk_giaoChiTieu = newGiao.id;
+                            db.bangDiems.Add(newBang);
+                            db.SaveChanges();
+                            break;
+                        }
+                    }
+                }
+            }
+            return RedirectToAction("Index");
         }
 
         // GET: loaiTieuChi/Delete/5
